b0306e9b6508836c4b1c77b09a2feaef6b38d5c9
immanikandan <immanikandan>
2021-02-08 05:38:02 +0000

    updated generated code

--- application/client/desktop/server/src/app/app.module.ts
+++ application/client/desktop/server/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { UserModule } from './user/user.module';
 import { HomeModule } from './home/home.module';
 import { SignupModule } from './signup/signup.module';
 import { LoginModule } from './login/login.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
@@ -18,15 +18,15 @@ import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
-        AppComponent
+            AppComponent
 ],
   imports: [
-  FormsModule,
-HttpClientModule,
-UserModule,
+  UserModule,
 HomeModule,
 SignupModule,
 LoginModule,
+  FormsModule,
+HttpClientModule,
 TemplateModule,
 FooterModule,
 HeaderModule,
@@ -34,10 +34,10 @@ TranslatorModule,
 AppRoutingModule,
 BrowserAnimationsModule,
       BrowserModule,
-AdminModule
+  AdminModule
 ],
   providers: [
-    	SharedService
+        	SharedService
 ],
   bootstrap: [AppComponent]
 })
